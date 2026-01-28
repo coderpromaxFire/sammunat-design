6a627ab8498b64f0086829b9d208bb4793450838
Kia <ccs@kias-macbook.local>
2026-01-28 13:43:16 +0530

    updated blogs

--- src/blog/useBlogs.js
+++ src/blog/useBlogs.js
@@ -1,24 +1,20 @@
 // src/blog/useBlogs.js
 
 import { useEffect, useState } from "react";
-
-const BLOG_API_URL = "https://script.google.com/macros/s/AKfycbxyj3LR3jpT_Q8gaZ51BhUYWF0ASS_-oR1TCBkVb_0rpRQOVG7-TA6a74JYuKRX8UU/exec";
+import blogData from "./blogData";
 
 export function useBlogs() {
   const [blogs, setBlogs] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch(BLOG_API_URL)
-      .then(res => res.json())
-      .then(data => {
-        setBlogs(data);
-        setLoading(false);
-      })
-      .catch(err => {
-        console.error("Error loading blogs:", err);
-        setLoading(false);
-      });
+    // simulate API delay (optional)
+    const timer = setTimeout(() => {
+      setBlogs(blogData);
+      setLoading(false);
+    }, 200);
+
+    return () => clearTimeout(timer);
   }, []);
 
   return { blogs, loading };
